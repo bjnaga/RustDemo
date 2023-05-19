6ad3a08646803226a5cafe0b3e89de675d1283d6
Naga Ganesh B J <bjnaga@gmail.com>
2023-05-19 17:40:36 +0530

    added scalar datatype , and compound types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
 fn main() {
+    let i = 10; // we cannot modify this value.
+    let mut ii =20;// we can modify this value.
+    const C:i8= 30; // const stay alive  till the program is alive.
     println!("Hello, world!");
+    let hex = 0xff;
+    let oct =0o377;
+    let bin=0b1111_1111;
+    let dec=02_55;
+    println!("{} {} {} {}",hex,oct, bin ,dec);
+//      rust had 4 scalar data types
+//     int char float and boolean
+// int can be i8,i16 , i 32 , i64 , i128
+//     int can be signed can unsigned
+    let inta:u8= 35;
+    println!("{}",inta);
+ let byte=b'A';
+    println!("{}",byte);
+    let _y=10; // here _ will silence the warning that we didnt use the variable y after declaring
+    let x = 2.0;
+    let xx:f32 =3.0;
+    let t=true;
+    let f:bool =false;
+     let other_c = 'c';
+    let a =10;
+    let b= 30;
+    let rem= a %b;
+    println!("{}",rem);
+// premitive compound type
+//     we have tuple and array
+//     tuple cannot be edited but could contain different datatype elements in it.
+    let tup = (10,"hi",345);
+    println!("{}",tup.0); // accessing content of a tuple
+        let (aaa,bbb,ccc)=tup;
+//    array will have length but will have same type in it by default , if we make it mutable it will work.
+    let arr=[1,3,4];
+    print!("{}",arr[2]);
+        
+
+
 }
